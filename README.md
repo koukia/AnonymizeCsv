14009529318125b61681a31cc10fe6722541a11c
mori <tk011875@gmail.com>
2019-05-25 20:21:48 +0900

    [fix]encode by csv INDEX

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Overview
 CSVファイルの特定の項目をハッシュ化することで匿名化する．
 
-## Eenvironment
+## Environment
 - python3系
 - 標準ライブラリのみ使用
 
@@ -15,3 +15,7 @@ CSVファイルの特定の項目をハッシュ化することで匿名化す�
    
 2. 以下を実行
    - `python3 csv_anonymizer.py`
+
+3. 匿名化するカラムの編集
+   1. 匿名化するカラムのインデックスをグローバル変数INDEX_{hoge}に定義.
+   2. 匿名化するインデックスをグローバル変数TARGET_INDEXESに定義.
--- csv_anonymizer.py
+++ csv_anonymizer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3.6
 # -*- coding: utf-8 -*-
 
-# palo alto log anonymization.
+# anonymize palo alto log .
 # IPなどをハッシュ化により匿名化する.
 # [CSV format](https://docs.paloaltonetworks.com/pan-os/7-1/pan-os-admin/monitoring/syslog-field-descriptions.html)
+
 import hashlib
 import csv
 
@@ -11,31 +12,42 @@ import csv
 # 'Traffic Log Fields', 'Threat Log Fields' に共通する項目.
 # test_target_keys_b = ["Source IP", "Destination IP", "Source User", "Destination User"]
 # test_target_keys = ['source_ip', 'destination_ip', 'source_user', 'destination_user']
-target_keys = ["src", "dst", "srcuser", "dstuser"]
+INDEX_TYPE = 3
+TRAFFIC = "TRAFFIC"
+INDEX_SRC_IP = 7
+INDEX_DST_IP = 8
+INDEX_SRC_USER = 12
+INDEX_DST_USER = 13
+TARGET_INDEXES = [INDEX_SRC_IP, INDEX_DST_IP, INDEX_SRC_USER, INDEX_DST_USER]
 
 
-def anonymize(csv_file, output_file, hash_targets):
-    is_first = True
-    
-    with open(output_file, 'w') as out_f:
-        with open(csv_file) as csv_f:
-            dict_reader = csv.DictReader(csv_f)
-            if is_first:
-                # print(dict_reader.fieldnames)
-                csv_fields = dict_reader.fieldnames
-                writer = csv.DictWriter(out_f, csv_fields)
-                writer.writeheader()
-            
-            for csv_dict in dict_reader:
-                # TODO: 内包表記でネスト浅くできる気がする.
-                for target in hash_targets:
-                    if csv_dict.get(target):
-                        # print(csv_dict.get(target))
-                        csv_dict[target] = hashlib.sha256(csv_dict[target].encode('utf-8')).hexdigest()
-                writer.writerow(csv_dict)
+def anonymize_traffic_log(logs):
+    if logs[INDEX_TYPE] != TRAFFIC:
+        print("[Error] Not TRAFFIC log.")
+        return None
+    for index in TARGET_INDEXES:
+        if logs[index]:
+            # print("[encode before]", logs[_index])
+            logs[index] = hashlib.sha256(logs[index].encode('utf-8')).hexdigest()
+            # print("[encode after]", logs[_index])
+
+    return logs
+
+
+def anonymize(target_csv_file, output_file):
+    with open(target_csv_file, 'r') as csv_f:
+        with open(output_file, 'w') as out_f:
+            reader = csv.reader(csv_f)
+            writer = csv.writer(out_f)
+
+            for fields_str in reader:
+                # print(fields_str)
+                fields = anonymize_traffic_log(fields_str)
+                writer.writerow(fields)
+
 
 if __name__ == '__main__':
-    csv_file = "paloaltologs/test_log.csv"
-    out_file = "paloaltologs/test_log_out.csv"
+    csv_file = "paloaltologs/log.csv"
+    out_file = "paloaltologs/log_out.csv"
 
-    anonymize(csv_file, out_file, target_keys)
+    anonymize(csv_file, out_file)
